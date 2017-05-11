b1aa66186b7b0a1b5d86b3c0eb3e460331b759c2
Andrew Fasano <hello@andrewfasano.com>
2017-05-10 21:52:36 -0700

    Now it's broken...

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ docker build -t bitcoin ./docker || exit
 
 docker run -it \
 -v "/home/${USER}":"/home/shadow/host" \
--v "/home/${USER}/bitcoin":"/home/shadow/bitcoin" \
 -v "/home/${USER}/.inputrc":"/home/shadow/.inputrc" \
 -v "/home/${USER}/.vimrc":"/home/shadow/.vimrc" \
 -v "/home/${USER}/.bashrc":"/home/shadow/.bashrc" \
