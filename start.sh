d31ba86b83988dfd2a4a461a33453037d362428e
Andrew Fasano <hello@andrewfasano.com>
2017-05-10 18:30:14 -0700

    Add 'run' alias, change command to run, map in .vimrc

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ docker run -it \
 -v "/home/${USER}":"/home/shadow/host" \
 -v "/home/${USER}/bitcoin":"/home/shadow/bitcoin" \
 -v "/home/${USER}/.inputrc":"/home/shadow/.inputrc" \
+-v "/home/${USER}/.vimrc":"/home/shadow/.vimrc" \
 -v "/home/${USER}/.bashrc":"/home/shadow/.bashrc" \
 bitcoin bash
